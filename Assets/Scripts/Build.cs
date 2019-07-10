6a866aa9bf6015564c9f99627a7d54fe37d987a4
isaevyaroslav <iyaroslav@yandex.ru>
2019-07-10 22:16:52 +0300

    Добавьте файлы проекта.

---
+++ Assets/Scripts/Build.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace LostInSpace
+{
+    public class Build : MonoBehaviour
+    {
+        public GameObject buildings;
+        public GameObject buildPlace;
+        public GameObject buildObject;
+
+        private Vector3 buildVector;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            buildVector = transform.position;
+            buildPlace = GameObject.Instantiate(buildPlace, transform.position, Quaternion.identity, transform);
+            buildPlace.SetActive(false);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.anyKeyDown)
+            {
+                SetBuildVector();
+                ShowBuildPlace();
+                BuildObject();
+            }
+            if (transform.gameObject.GetComponent<MovePlayer>().IsMoving)
+            {
+                HideBuildPlace();
+            }
+    }
+
+        private void SetBuildVector()
+        {
+            Vector3 currentBuildVector = transform.position;
+            if (Input.GetKey(KeyCode.W))
+            {
+                currentBuildVector.z -= 1;
+            }
+            else if (Input.GetKey(KeyCode.D))
+            {
+                currentBuildVector.x -= 1;
+            }
+            else if (Input.GetKey(KeyCode.S))
+            {
+                currentBuildVector.z += 1;
+            }
+            else if (Input.GetKey(KeyCode.A))
+            {
+                currentBuildVector.x += 1;
+            }
+            if(currentBuildVector != transform.position)
+            {
+                buildVector = currentBuildVector;
+            }
+        }
+        private void BuildObject()
+        {
+            if (Input.GetKey(KeyCode.Return) && buildVector != transform.position)
+            {
+                GameObject.Instantiate(buildObject, buildVector, Quaternion.identity, buildings.transform);
+                buildVector = transform.position;
+                HideBuildPlace();
+            }
+        }
+        private void HideBuildPlace()
+        {
+            buildPlace.SetActive(false);
+            buildPlace.transform.position = transform.position;
+        }
+        private void ShowBuildPlace()
+        {
+            if(buildVector != transform.position)
+            {
+                buildPlace.SetActive(true);
+                buildPlace.transform.position = buildVector;
+            }
+        }
+    }
+}
+
+
---
+++ Assets/Scripts/MovePlayer.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace LostInSpace
+{
+
+    public class MovePlayer : MonoBehaviour
+    {
+        public bool IsMoving = false;
+        // Use this for initialization
+        void Awake()
+        {
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            IsMoving = false;
+            Vector3 moveVector = Vector3.zero;
+            if (Input.anyKeyDown)
+            {
+                if (Input.GetKey(KeyCode.UpArrow))
+                {
+                    moveVector.z -= 1;
+                }
+                else if (Input.GetKey(KeyCode.RightArrow))
+                {
+                    moveVector.x -= 1;
+                }
+                else if (Input.GetKey(KeyCode.DownArrow))
+                {
+                    moveVector.z += 1;
+                }
+                else if (Input.GetKey(KeyCode.LeftArrow))
+                {
+                    moveVector.x += 1;
+                }
+                if (moveVector != Vector3.zero)
+                {
+                    Ray ray = new Ray(transform.position, moveVector);
+                    Physics.Raycast(ray, out RaycastHit hit);
+                    if (hit.collider == null || hit.distance > 1)
+                    {
+                        IsMoving = true;
+                        transform.position = transform.position + moveVector;
+                    }
+                }
+            }
+        }
+    }
+
+}
